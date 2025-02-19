7051d822facd688bbd4529ad461b0a5f8fd9626f
ripionck <ripion.chakmar@gmail.com>
2025-02-19 08:19:38 +0600

    added loading to analytics

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -183,10 +183,9 @@ export default function Analytics() {
         </div>
       )}
 
-      {/* Loading State */}
       {loading && (
-        <div className="text-center py-6 text-gray-600">
-          Loading financial data...
+        <div className="flex justify-center items-center h-64">
+          <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
         </div>
       )}
 
